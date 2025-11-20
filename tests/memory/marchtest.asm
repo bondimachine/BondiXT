458d3189517ad20595c1781b57c8980fb4bcaf1a
Ariel Scarpinelli <triforcexp@gmail.com>
2025-11-20 13:14:35 -0300

    finish marchtest

--- tests/memory/marchtest.asm
+++ tests/memory/marchtest.asm
@@ -3,24 +3,29 @@ BITS 16
 P0 EQU 0b01010101
 P1 EQU 0b10101010
 
+; %define QEMU
+
 times 0xE000 - ($ - $$) db 0 ; put the code in the last 8kb
 
 start:
 
+
 ; March C- algorithm {↑ (w0); ↑ (r0, w1); ↑ (r1, w0); ↓ (r0, w1); ↓ (r1, w0); ↓ (r0)}
 
+    cli
     mov bp, 0
     mov es, bp
 
     mov al, 1
     mov si, _phase1
-    jmp print_nibble_wait
+    jmp print_phase
 
 _phase1:
     mov di, 0
     mov al, P0 
     mov cx, 0xFFFF
     rep stosb
+    stosb
 
     mov ax, bp
     ; poormans "stack"
@@ -29,7 +34,7 @@ _phase1:
 
 _continue_phase1:
     inc bp
-    cmp bp, 11
+    cmp bp, 10
     je _done_phase1
 
     mov ax, bp
@@ -42,7 +47,7 @@ _done_phase1:
 
     mov al, 2
     mov si, _phase2
-    jmp print_nibble_wait
+    jmp print_phase
     
 _phase2:
 ; Phase 2: ↑ (r0, w1)
@@ -56,7 +61,7 @@ _phase2:
 _done_phase2:
     mov al, 3
     mov si, _phase3
-    jmp print_nibble_wait
+    jmp print_phase
     
 _phase3:
 ; Phase 3: ↑ (r1, w0)
@@ -70,8 +75,8 @@ _phase3:
 _done_phase3:
 
     mov al, 4
-    mov si, _phase3
-    jmp print_nibble_wait
+    mov si, _phase4
+    jmp print_phase
     
 _phase4:
 
@@ -84,9 +89,9 @@ _phase4:
 
 _done_phase4:
 
-    mov al, 4
+    mov al, 5
     mov si, _phase5
-    jmp print_nibble_wait
+    jmp print_phase
     
 _phase5:
 ; Phase 5: ↓ (r1, w0)
@@ -100,7 +105,7 @@ _done_phase5:
 
     mov al, 6
     mov si, _phase6
-    jmp print_nibble_wait
+    jmp print_phase
     
 _phase6:
 
@@ -112,9 +117,8 @@ _phase6:
 
 success:
     ; all phases complete - success
-    mov al, 'S'
     mov si, halt
-    jmp print
+    jmp print_success
 
 march_up:
     mov bp, 0
@@ -123,7 +127,7 @@ march_up:
 .outer_loop:
 
     mov di, 0
-    mov cx, 0xFFFF
+    mov cx, 0
 
 .inner_loop:
     mov al, [es:di]
@@ -141,7 +145,7 @@ march_up:
 .continue:
 
     inc bp
-    cmp bp, 11
+    cmp bp, 10
     je .done
 
     mov ax, bp
@@ -154,15 +158,15 @@ march_up:
 
 
 march_down:
-    mov bp, 10
+    mov bp, 9
     mov ax, bp
-    rol ax, 10
+    rol ax, 12
     mov es, ax
 
 .outer_loop:
 
     mov di, 0xFFFF
-    mov cx, 0xFFFF
+    mov cx, 0
 
 .inner_loop:
     mov al, [es:di]
@@ -179,8 +183,9 @@ march_down:
 
 .continue:
 
+    cmp bp, 0
+    je .done
     dec bp
-    jz .done
 
     mov ax, bp
     rol ax, 12
@@ -190,8 +195,9 @@ march_down:
 .done:
     jmp sp
 
+%ifdef QEMU
 ; prints lower 4 bits of al
-; destroys si, dx, al
+print_nibble_error:
 print_nibble:
     and al, 0x0F
     cmp al, 10
@@ -199,18 +205,53 @@ print_nibble:
     add al, 'A' - 10 - '0'
 .not_bigger:
     add al, '0'
+    jmp print
 
+print_blink:
+    mov al, '_'
+    jmp print
+
+print_error:
+    mov al, 'E'
+    jmp print
+
+print_success:
+    mov al, 'S'
+    jmp print
+%else 
+
+print_nibble_error:
+    or al, 0x40
+print_nibble:
+    jmp print
+
+print_blink:
+    mov al, 0
+    jmp print
+
+print_error:
+    mov al, 0x40
+    jmp print
+
+print_success:
+    mov al, 0xFF
+    jmp print
+
+%endif
+
+
+; destroys si, dx, al
 print:
     mov dx, 0x378
     out dx, al
 
+%ifdef QEMU
     ; this is only needed for QEMU to print on the screen
     mov dx, 0x37A
     mov al, 0x08 | 0x04 | 1 ; 0x08 = select, 0x04 = !initialize, 1 = strobe
     out dx, al
     mov al, 0x08 | 0x04
     out dx, al
-
 
     mov al, 13
     mov dx, 0x378
@@ -232,17 +273,23 @@ print:
     out dx, al
     mov al, 0x08 | 0x04
     out dx, al
+%endif
 
     jmp si
 
 ; prints al (as a character) to parallel port 0x378 with wait/blink
 ; destroys si, dx, cx, sp, al
-print_nibble_wait:
+print_phase:
     mov sp, si
+%ifndef QEMU
+    mov cl, al 
+    mov al, 1
+    rol al, cl
+    or al, 0x80
+%endif
     mov bl, al
-    mov al, '_'
     mov si, .blink0
-    jmp print
+    jmp print_blink
 .blink0:
     mov si, .wait0
     jmp _wait
@@ -254,9 +301,8 @@ print_nibble_wait:
     mov si, .wait1
     jmp _wait
 .wait1:    
-    mov al, '_'
     mov si, .blink2
-    jmp print
+    jmp print_blink
 .blink2:
     mov si, .wait2
     jmp _wait
@@ -268,9 +314,8 @@ print_nibble_wait:
     mov si, .wait3
     jmp _wait
 .wait3:    
-    mov al, '_'
     mov si, .blink4
-    jmp print
+    jmp print_blink
 .blink4:
     mov si, .wait4
     jmp _wait
@@ -287,9 +332,8 @@ _wait:
 
 error:
     ; error - print 'E' and halt
-    mov al, 'E'
     mov si, .blink1
-    jmp print
+    jmp print_error
 .blink1:
     mov si, .wait1
     jmp _wait
@@ -298,7 +342,7 @@ error:
     mov ax, es
     ror ax, 11
     mov si, .blink2
-    jmp print_nibble
+    jmp print_nibble_error
 .blink2:
     mov si, .wait2
     jmp _wait
@@ -308,7 +352,7 @@ error:
     mov al, ah
     ror al, 4
     mov si, .blink3
-    jmp print_nibble
+    jmp print_nibble_error
 .blink3:
     mov si, .wait3
     jmp _wait
@@ -317,7 +361,7 @@ error:
     mov ax, di
     mov al, ah
     mov si, .blink4
-    jmp print_nibble
+    jmp print_nibble_error
 .blink4:
     mov si, .wait4
     jmp _wait
@@ -326,15 +370,16 @@ error:
     mov ax, di
     ror al, 4
     mov si, .blink5
-    jmp print_nibble
+    jmp print_nibble_error
 .blink5:
     mov si, .wait5
     jmp _wait
 .wait5:
     ; es:di failing address
     mov ax, di
+    and al, 0x0F
     mov si, .blink6
-    jmp print_nibble
+    jmp print_nibble_error
 .blink6:
     mov si, .wait6
     jmp _wait
@@ -342,14 +387,15 @@ error:
     mov al, [es:di]
     ror al, 4
     mov si, .blink7
-    jmp print_nibble
+    jmp print_nibble_error
 .blink7:
     mov si, .wait7
     jmp _wait
 .wait7:
     mov al, [es:di]
+    and al, 0x0F
     mov si, halt
-    jmp print_nibble
+    jmp print_nibble_error
 
 halt:    
     hlt
