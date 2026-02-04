86166666fdb0802777e237913dcc21c6a809f874
Ariel Scarpinelli <triforcexp@gmail.com>
2026-02-04 18:55:46 -0300

    advanced basic memory test

--- tests/memory/basictest.asm
+++ tests/memory/basictest.asm
@@ -12,37 +12,89 @@ start:
     mov ax, 0
     mov ds, ax
     mov si, 0
+    mov cl, 1
 
     mov dx, 0x378
+    mov al, cl
+    out dx, al
 
 continue:
     mov al, P0
     mov [ds:si], al
     mov al, [ds:si]
-    out dx, al
-
-    mov cx, 0xFFFF
-.loop:    
-    dec cx
-    jnz .loop
+    cmp al, P0
+    jne error
 
     mov al, P1
     mov [ds:si], al
     mov al, [ds:si]
-    out dx, al
-
-    mov cx, 0xFFFF
-.loop2:    
-    dec cx
-    jnz .loop2
+    cmp al, P1
+    jne error
 
     inc si
     jnz continue
+    inc cl
+    cmp cl, 11
+    je done
     mov ax, ds
     add ax, 0x1000
     mov ds, ax
 
+    mov al, cl
+%ifdef QEMU
+    add al, '0'
+%endif
+    out dx, al
+
+%ifdef QEMU
+    mov dx, 0x37A
+    mov al, 0x08 | 0x04 | 1 ; 0x08 = select, 0x04 = !initialize, 1 = strobe
+    out dx, al
+    mov al, 0x08 | 0x04
+    out dx, al
+    mov dx, 0x378
+%endif
+
     jmp continue
+
+error:
+    mov al, cl
+%ifdef QEMU
+    add al, 'A'
+%else
+    or al, 0x80
+%endif
+    out dx, al
+
+%ifdef QEMU
+    mov dx, 0x37A
+    mov al, 0x08 | 0x04 | 1 ; 0x08 = select, 0x04 = !initialize, 1 = strobe
+    out dx, al
+    mov al, 0x08 | 0x04
+    out dx, al
+    mov dx, 0x378
+%endif
+
+    hlt
+
+done:
+%ifdef QEMU
+    mov al, 'S'
+%else
+    mov al, 0b11011011
+%endif
+    out dx, al
+
+%ifdef QEMU
+    mov dx, 0x37A
+    mov al, 0x08 | 0x04 | 1 ; 0x08 = select, 0x04 = !initialize, 1 = strobe
+    out dx, al
+    mov al, 0x08 | 0x04
+    out dx, al
+    mov dx, 0x378
+%endif
+
+    hlt
 
 reset_vector:
     times 0xFFF0 - ($ - $$) db 0
